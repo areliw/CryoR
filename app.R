68e930441bb220ad5fcf6895b56a34abfc867dd5
areliw <149886917+areliw@users.noreply.github.com>
2024-09-12 15:28:09 +0700

    Update app.R

--- app.R
+++ app.R
@@ -1,6 +1,7 @@
 library(shiny)
 source("ui.R")     # โหลดไฟล์ UI
 source("server.R") # โหลดไฟล์ Server
+source("global.R") # โหลดค่าตัวแปรและแพ็กเกจร่วม
 
 # รันแอป Shiny
 shinyApp(ui = ui, server = server)
