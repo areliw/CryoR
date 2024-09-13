9ecb21ce6284adbdc53984d2e79fa56b6f92f426
areliw <149886917+areliw@users.noreply.github.com>
2024-09-14 01:02:50 +0700

    Update correlation_analysis.R

--- modules/correlation_analysis.R
+++ modules/correlation_analysis.R
@@ -1,13 +1,19 @@
 # modules/correlation_analysis.R
 
 correlation_analysis_module <- function(input, output, data_clean) {
+  correlation_result <- reactiveVal(NULL)
+
   observeEvent(input$analyze_correlation, {
     data <- data_clean()
     req(data)
-    
-    correlation_result <- cor(as.numeric(data$`Cryo Volume (ml/unit)`), as.numeric(data$`FFP Volume (ml)`), use = "complete.obs")
-    output$correlation_result <- renderText({
-      paste("Correlation between Cryo Volume and FFP Volume:", round(correlation_result, 4))
-    })
+
+    result <- cor(data$`Cryo Volume (ml/unit)`, data$`FFP Volume (ml)`, use = "complete.obs")
+    correlation_result(result)
+  })
+
+  output$correlation_result <- renderText({
+    result <- correlation_result()
+    req(result)
+    paste("Correlation between Cryo Volume and FFP Volume:", round(result, 4))
   })
 }
