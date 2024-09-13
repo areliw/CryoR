739890d9f65890da55ba4fe169e2e3a1303bc58e
areliw <149886917+areliw@users.noreply.github.com>
2024-09-14 01:49:57 +0700

    Update plots.R

--- modules/plots.R
+++ modules/plots.R
@@ -1,48 +1,47 @@
 # modules/plots.R
 
 plots_module <- function(input, output, data_clean) {
-  # Q-Q plot for Cryo Volume
-  output$qq_plot <- renderPlot({
-    data <- data_clean()
-    req(data)
+    # Q-Q plot for Cryo Volume
+    output$qq_plot <- renderPlot({
+        data <- data_clean()
+        req(data)
 
-    ggplot(data, aes(sample = `Cryo Volume (ml/unit)`)) +
-      stat_qq() +
-      stat_qq_line() +
-      labs(title = "Q-Q Plot for Cryo Volume",
-           x = "Theoretical Quantiles", y = "Sample Quantiles") +
-      theme_minimal()
-  })
-
-  # Histogram of Cryo Volume by Time Interval
-  observeEvent(input$plot_histogram, {
-    data <- data_clean()
-    req(data)
-
-    # แปลงคอลัมน์เป็นตัวเลขและลบค่า NA
-    data <- data[!is.na(data$`Cryo Volume (ml/unit)`), ]
-    data$`Cryo Volume (ml/unit)` <- as.numeric(data$`Cryo Volume (ml/unit)`)
-
-    # สร้างกราฟ Histogram แยกตาม Time Interval
-    output$histogram_plot <- renderPlot({
-      ggplot(data, aes(x = `Cryo Volume (ml/unit)`)) +
-        geom_histogram(binwidth = 1, fill = "skyblue", color = "black") +
-        facet_wrap(~ time_interval, ncol = 2, scales = "free_y") +
-        labs(title = "Histogram of Cryo Volume by Time Interval",
-             x = "Cryo Volume (ml/unit)", y = "Count") +
-        theme_minimal()
+        ggplot(data, aes(sample = `Cryo Volume (ml/unit)`)) +
+            stat_qq() +
+            stat_qq_line() +
+            labs(title = "Q-Q Plot for Cryo Volume",
+                 x = "Theoretical Quantiles", y = "Sample Quantiles") +
+            theme_minimal()
     })
-  })
 
-  # Scatter Plot
-  output$scatter_plot <- renderPlot({
-    data <- data_clean()
-    req(data)
+    # Histogram of Cryo Volume by Time Interval
+    observeEvent(input$plot_histogram, {
+        data <- data_clean()
+        req(data)
 
-    ggplot(data, aes(x = `FFP Volume (ml)`, y = `Cryo Volume (ml/unit)`)) +
-      geom_point() +
-      labs(title = "Scatter Plot of Cryo Volume vs FFP Volume",
-           x = "FFP Volume (ml)", y = "Cryo Volume (ml/unit)") +
-      theme_minimal()
-  })
+        # ลบค่า NA ในคอลัมน์ที่จำเป็น
+        data <- data[!is.na(`Cryo Volume (ml/unit)`)]
+        
+        # สร้างกราฟ Histogram แยกตาม Time Interval
+        output$histogram_plot <- renderPlot({
+            ggplot(data, aes(x = `Cryo Volume (ml/unit)`)) +
+                geom_histogram(binwidth = 1, fill = "skyblue", color = "black") +
+                facet_wrap(~ time_interval, ncol = 2, scales = "free_y") +
+                labs(title = "Histogram of Cryo Volume by Time Interval",
+                     x = "Cryo Volume (ml/unit)", y = "Count") +
+                theme_minimal()
+        })
+    })
+
+    # Scatter Plot
+    output$scatter_plot <- renderPlot({
+        data <- data_clean()
+        req(data)
+
+        ggplot(data, aes(x = `FFP Volume (ml)`, y = `Cryo Volume (ml/unit)`)) +
+            geom_point() +
+            labs(title = "Scatter Plot of Cryo Volume vs FFP Volume",
+                 x = "FFP Volume (ml)", y = "Cryo Volume (ml/unit)") +
+            theme_minimal()
+    })
 }
